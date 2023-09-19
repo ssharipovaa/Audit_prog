ed53cedf6d3b1c9d9303ec7ce0bef7dbb16cc546
ssharipovaa <kimmihrebekka@gmail.com>
2023-09-19 22:16:23 +0500

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Audit.Pages;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Audit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public string login = "";
+        public string password = "A181818a";
+        public MainWindow()
+        {
+            InitializeComponent();
+            f_Content.Content = new Calculate();
+            tb_Name.Text = $"";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+           this.Close();
+        }
+
+    }
+
+}
---
+++ Pages/Calculate.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Audit.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Calculate.xaml
+    /// </summary>
+    public partial class Calculate : Page
+    {
+        public Calculate()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int num1 = int.Parse(tbx1.Text);
+                if (num1 > 3)
+                {
+                    MessageBox.Show("Введите значение от 1 до 3");
+                    tbx1.Text = "0";
+                    return;
+                }
+                int num2 = int.Parse(tbx2.Text);
+                int num3 = int.Parse(tbx3.Text);
+                int num4 = int.Parse(tbx4.Text);
+                if (num1 > 2)
+                {
+                    MessageBox.Show("Очный аудит");
+                }
+                else
+                {
+                    if (num2 < 549)
+                    {
+                        MessageBox.Show("Очный аудит");
+                    }
+                    else
+                    {
+                        if (num3 < 2)
+                        {
+                            MessageBox.Show("Очный аудит");
+                        }
+                        else
+                        {
+                            if (num4 > 2)
+                            {
+                                MessageBox.Show("Очный аудит");
+                            }
+                            else 
+                                MessageBox.Show("Дистанционный аудит");
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введите корректные данные!");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
